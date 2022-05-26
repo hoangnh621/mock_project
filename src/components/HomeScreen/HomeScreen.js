737cc0b47d40e36538c9730f651100381440d0ad
Nguyen Khac Thien <thiennk@vnext.com.vn>
2022-05-26 15:22:10 +0700

    building page 404 and routing

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,6 +1,15 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HomeScreen = () => {
+  const navigate = useNavigate()
+  const token = ''
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [])
   return <div>HomeScreen</div>
 }
 
