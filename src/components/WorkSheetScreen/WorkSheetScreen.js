79f649c6f8fc454eca1fc51ef6bd12a3e72e252a
thanhtd <thanhtd@vnext.vn>
2022-06-01 15:23:25 +0700

    feat/popup_over_time

--- src/components/WorkSheetScreen/WorkSheetScreen.js
+++ src/components/WorkSheetScreen/WorkSheetScreen.js
@@ -1,5 +1,44 @@
+import { Button, Modal } from 'antd'
+import 'antd/dist/antd.min.css'
+import React, { useState } from 'react'
+import OverTime from '../../common/popup/OverTime/OverTime'
+import './WorkSheetScreen.scss'
+
 const WorkSheet = () => {
-  return <div>WorkSheet</div>
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
+  return (
+    <div>
+      <div> WorkSheet </div>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Register OT"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width="80%"
+        okText="Register"
+        okButtonProps={{ style: { marginRight: '20px' } }}
+        className="modal_ot"
+      >
+        <OverTime />
+      </Modal>
+    </div>
+  )
 }
 
 export default WorkSheet
