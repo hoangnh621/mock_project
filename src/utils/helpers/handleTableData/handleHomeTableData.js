df1b7d13dce9bedabc26cf180042613c61f8b181
Nguyen Huy Hoang 2 <hoangnh2@vnext.vn>
2022-06-15 16:34:41 +0700

    feat: download and view file

--- src/utils/helpers/handleTableData/handleHomeTableData.js
+++ src/utils/helpers/handleTableData/handleHomeTableData.js
@@ -1,8 +1,9 @@
 import moment from 'moment'
 
-const handleHomeTableData = (dataNotice) => {
+const handleHomeTableData = (dataNotice, officialNotice, orderType) => {
   const dataSource = dataNotice?.map((notice, index) => {
     const publishedTo = notice?.published_to
+    // Handle division name
     let toDepartment = ''
     const publishedDate = moment(notice.published_date).format('DD/MM/YYYY')
     if (typeof publishedTo === 'string') {
@@ -10,9 +11,19 @@ const handleHomeTableData = (dataNotice) => {
     } else {
       toDepartment = notice?.published_to[0].division_name
     }
+    //Handle table no
+    const page = officialNotice?.current_page
+    const perPage = officialNotice?.per_page
+    const total = officialNotice?.total
+    let tableNo = null
+    if (orderType === 'desc') {
+      tableNo = (page - 1) * perPage + index + 1
+    } else {
+      tableNo = total - ((page - 1) * perPage + index)
+    }
     return {
       key: +index,
-      no: index + 1,
+      no: tableNo,
       subject: notice.subject,
       author: notice.author.full_name,
       toDepartment: toDepartment,
